7b9e666428b7c67b197ecb6ee2a4c4d9a8e543ac
Mo Moadeli <megabyzus@gmail.com>
2024-01-09 06:19:58 -0500

    comments

--- src/app/shared/ods-state.ts
+++ src/app/shared/ods-state.ts
@@ -5,14 +5,14 @@ import { distinctUntilChanged, map } from 'rxjs/operators';
   name: momo
   concept: 
     The Observable Data Store (ODS) is a base class for all services that provide 'local/global/component' state for a feature.
-    Key is this state can run within the liecycle of the client smart component and doesn't need to usurp global memory unlike an
+    A key feature is this object can run within the lifecycle of the client smart component and doesn't need to usurp global memory unlike an
     NgRx slice that stays in memory during the lifetime of the application.
   intent: 
     This is a base class for all services that that provide 'local global' state for a feature.
     This service is also gateway to all modes of data such as API responses, local storage, ngrx store, 
-    etc. Additionally an ODS state cab be easily promoted to a global ngRx slice once and if needed.
-    As a service, it can be injected into any component.It can also be overridden
-    using Angular's DI mechanism in case an alternative implementation is needed.
+    etc. Additionally an ODS state can be easily promoted to a global ngRx slice once and if needed.
+    As a service, it can be injected into any component. 
+    It can also be overridden using Angular's DI mechanism in case an alternative implementation is needed.
 */
 
 export class ODSState<T> {
